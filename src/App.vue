041b47d671b58ec27adc65b3414dcdb7ce3627b6
Minato3000 <101650634+Minato3000@users.noreply.github.com>
2022-05-09 21:13:26 +0530

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Main />
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'App',
+  components: { Main },
+  data() {
+    return {
+      title: 'Anime Sensei'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
